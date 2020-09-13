fc26ddd11897e4e2175901691c251a7989dcc71a
23ksw10 <kimsw9603@gmail.com>
2020-09-13 23:43:49 +0900

    C++

---
+++ 1221_Split_a_String_in_Balanced_Strings/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        ans = 0
+        r = 0
+        l = 0
+        for c in s:
+            if c == 'R':
+                r += 1
+            else:
+                l += 1
+            if r == l:
+                ans += 1
+                r = 0
+                l = 0
+
+        return ans
