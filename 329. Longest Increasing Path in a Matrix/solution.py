2db9efc65bdee8d8b32654c89c0b88e53bf64b2c
Kim sun wook <50804079+23ksw10@users.noreply.github.com>
2021-03-17 00:50:18 +0900

    Update solution.py

--- 329. Longest Increasing Path in a Matrix/solution.py
+++ 329. Longest Increasing Path in a Matrix/solution.py
@@ -3,11 +3,12 @@ class Solution:
         ans = 0
         ud = [-1,0,1,0]
         lr = [0,1,0,-1]
-        
         m = len(matrix)
         n = len(matrix[0])
-        @functools.cache
+        dp = [[0]*n for i in range(m)]
         def dfs(i,j):
+            if dp[i][j]:
+                return dp[i][j]
             count = 0
             for k in range(4):
                 next_i = i+ud[k]
@@ -15,9 +16,9 @@ class Solution:
                 if next_i>=0 and next_i<m and next_j>=0 and next_j<n:
                     if matrix[next_i][next_j]>matrix[i][j]:
                         count =max(count,dfs(next_i,next_j))
-            return count+1
+            dp[i][j] = count+1
+            return dp[i][j]
         for i in range(m):
             for j in range(n):
                 ans=max(ans,dfs(i,j))
         return ans
-                    
